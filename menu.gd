0d7308d421f2affddd1dd0a13108d8bc039a6b69
Jacob Yuill <jacobyuill@icloud.com>
2025-04-24 12:11:58 -0400

    add credits scene, placeholder cobble texture

--- menu.gd
+++ menu.gd
@@ -5,7 +5,13 @@ extends Control
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main.tscn")
+	
+	
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://credits.tscn")

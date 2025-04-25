9ada530f36a2ad6445776a0a8084fce3977de141
Jacob Yuill <jacobyuill@icloud.com>
2025-04-25 15:07:44 -0400

    unscuff credits scene

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -6,12 +6,8 @@ extends Control
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main.tscn")
 	
-	
-
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://credits.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
-
-
-func _on_credits_pressed() -> void:
-	get_tree().change_scene_to_file("res://credits.tscn")
